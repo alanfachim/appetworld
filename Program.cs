0ce5f1848f4176fd882080b94971fcd9363ddd33
alanfachim <alanffachim@gmail.com>
2021-05-10 14:25:43 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Carimbador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Negocio n = new Negocio();
+            n.Processar();
+            Console.WriteLine("Hello World!");
+            var db = new DAO();
+            var a = db.getGrupParameters("0001", null).Result;
+            var b = db.buscaRegras("51;1=1;2=1;", null).Result;
+            var c = db.buscaPontoCurva("1111", "20210505", "210507", null).Result;
+
+            
+        }
+    }
+}
